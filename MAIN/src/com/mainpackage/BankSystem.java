0e5e362d64889204fe3468f788c824bd4ec485ea
REMEMBER345 <rememberjoselyne345@gmail.com>
2024-11-11 21:37:05 +0300

    #Commit3
    Created method displayUserDetails to output user information

---
+++ MAIN/src/com/mainpackage/BankSystem.java
@@ -0,0 +1,4 @@
+package com.mainpackage;
+
+public class BankSystem {
+}
--- USER/src/com/userpackage/Client.java
+++ USER/src/com/userpackage/Client.java
@@ -1,4 +1,17 @@
 package com.userpackage;
-
+import com.userpackage.Client;
 public class Client {
+    String clientName;
+    int clientId;
+    int registrationCode;
+    String address;
+    public void displayUserDetails(String clientName, int clientId, int registrationCode, String address)
+    {
+       System.out.println("\nUser Details: ");
+        System.out.println("Client Name:" + clientName);
+        System.out.println("Client Id: "+ clientId);
+        System.out.println("Registration code: " + registrationCode);
+        System.out.println("Client address: " + address);
+    }
+
 }
